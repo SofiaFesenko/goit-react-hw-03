a4da4c790ccff4bbe4365196b70bb9bdd439deb2
Sofia Fesenko <sofia.fesen@gmail.com>
2024-08-07 19:57:47 +0300

    dfewf

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,13 @@
+function Contact({onDelete, name, number, id}) {
+    return (
+        <div className={css.contactBlock}>
+            <div>
+            <span>🎅 {name}</span> <br/>
+            <span>📞 {number}</span> 
+            </div>
+            <button type="button" onClick={() => onDelete(id)}>delete</button> 
+        </div>        
+    )
+}
+
+export default Contact
